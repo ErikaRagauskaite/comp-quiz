8bb46b83b84627da53ccb905bf222faac7ffaa94
ErikaRagauskaite <erika.ragauskaite99@gmail.com>
2024-10-16 17:21:11 +0100

    hope is a dangerous thing

--- comp-quiz/vite.config.js
+++ comp-quiz/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: ".", // Ensure the root is set correctly
+  build: {
+    rollupOptions: {
+      input: "index.html", // Ensure the entry point is set to index.html
+    },
+  },
+});
